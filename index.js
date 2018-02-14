2b86429a50ee8cd6cc075848ae1b181ace70b945
Web Development <webdevelopment@Administrators-Mac-mini.local>
2018-02-14 14:21:52 -0500

    changed video object

--- index.js
+++ index.js
@@ -1,22 +1,57 @@
-import video1 from './Phase1.mp4'
-import video2 from './Phase2.mp4'
-import video3 from './Phase3.mp4'
-import video4 from './Phase4.mp4'
-import video5 from './Phase5.mp4'
-import video6 from './Phase6.mp4'
-import video7 from './Phase7.mp4'
-import video8 from './Phase8.mp4'
-import video9 from './Phase9.mp4'
-import video10 from './Phase10.mp4'
-import video11 from './Phase11.mp4'
-import video12 from './Phase12.mp4'
-import video13 from './Phase13.mp4'
-import video14 from './Phase14.mp4'
-import video15 from './Phase15.mp4'
-import video16 from './Phase16.mp4'
+import video_1 from './Phase1.mp4'
+import video_2 from './Phase2.mp4'
+import video_3 from './Phase3.mp4'
+import video_4 from './Phase4.mp4'
+import video_5 from './Phase5.mp4'
+import video_6 from './Phase6.mp4'
+import video_7 from './Phase7.mp4'
+import video_8 from './Phase8.mp4'
+import video_9 from './Phase9.mp4'
+import video_10 from './Phase10.mp4'
+import video_11 from './Phase11.mp4'
+import video_12 from './Phase12.mp4'
+import video_13 from './Phase13.mp4'
+import video_14 from './Phase14.mp4'
+import video_15 from './Phase15.mp4'
+import video_16 from './Phase16.mp4'
 
-const videos = {}
-videos.video1
+const videos =
+{
+  video1: video_1,
+  video2: video_2,
+  video3: video_3,
+  video4: video_4,
+  video5: video_5,
+  video6: video_6,
+  video7: video_7,
+  video8: video_8,
+  video9: video_9,
+  video10: video_10,
+  video11: video_11,
+  video12: video_12,
+  video13: video_13,
+  video14: video_14,
+  video15: video_15,
+  video16: video_16,
+  video17: video_1,
+  video18: video_16,
+  video19: video_15,
+  video20: video_14,
+  video21: video_13,
+  video22: video_12,
+  video23: video_11,
+  video24: video_10,
+  video25: video_9,
+  video26: video_8,
+  video27: video_7,
+  video28: video_6,
+  video29: video_5,
+  video30: video_4,
+  video31: video_3,
+  video32: video_2
+}
+
+/*videos.video1
 videos.video2
 videos.video3
 videos.video4
@@ -31,6 +66,6 @@ videos.video12
 videos.video13
 videos.video14
 videos.video15
-videos.video16
+videos.video16*/
 
 export default videos
