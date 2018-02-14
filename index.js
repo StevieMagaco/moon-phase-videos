af5ccf165de090611c5b940a1b4f0e4cdf5d29c4
Web Development <webdevelopment@Administrators-Mac-mini.local>
2018-02-14 11:57:44 -0500

    added videos

---
+++ index.js
@@ -0,0 +1,36 @@
+import video1 from './Phase1.mp4'
+import video2 from './Phase2.mp4'
+import video3 from './Phase3.mp4'
+import video4 from './Phase4.mp4'
+import video5 from './Phase5.mp4'
+import video6 from './Phase6.mp4'
+import video7 from './Phase7.mp4'
+import video8 from './Phase8.mp4'
+import video9 from './Phase9.mp4'
+import video10 from './Phase10.mp4'
+import video11 from './Phase11.mp4'
+import video12 from './Phase12.mp4'
+import video13 from './Phase13.mp4'
+import video14 from './Phase14.mp4'
+import video15 from './Phase15.mp4'
+import video16 from './Phase16.mp4'
+
+const videos = {}
+videos.video1
+videos.video2
+videos.video3
+videos.video4
+videos.video5
+videos.video6
+videos.video7
+videos.video8
+videos.video9
+videos.video10
+videos.video11
+videos.video12
+videos.video13
+videos.video14
+videos.video15
+videos.video16
+
+export default videos
